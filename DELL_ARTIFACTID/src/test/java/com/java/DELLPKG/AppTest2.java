61f5fe6bb07829dbdbb8531b205dd47b37b52fa5
Ranjan <usert5558.gmail.com>
2019-06-08 15:34:18 +0000

    3 and 4 test

---
+++ DELL_ARTIFACTID/src/test/java/com/java/DELLPKG/AppTest2.java
@@ -0,0 +1,11 @@
+package com.java.DELLPKG;
+import org.testng.annotations.Test;
+
+public class AppTest1 
+{
+    @Test
+    public void  unitLevel1()
+    {
+	System.out.println("testClass: Unit level1 testing22222222");
+    }
+}
